c0f3c5c79f180d409097896777105aec01cc43a7
Jan Jachnik <jrjachnik@gmail.com>
2015-11-11 16:12:23 +0000

    Added exception handling

--- archUpdateNotifier.py
+++ archUpdateNotifier.py
@@ -10,15 +10,15 @@ import subprocess
 # Pacman for system updates
 # Yaourt for AUR updates
 
-#TODO add error checking on system calls
-
 update_check_interval_minutes = 120
 
 def getAURUpdates():
     command = "yaourt -Qua"
-    #output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    output = subprocess.check_output(command,shell=True).decode("utf-8").split('\n')
-    print(output)
+    try:
+        output = subprocess.check_output(command,shell=True).decode("utf-8").split('\n')
+    except subprocess.CalledProcessError as e:
+        print(e.cmd, "exited with non-zero exit code: ", e.returncode, file=sys.stderr)
+        return []
     updates = []
     for line in output:
         if line!='':
@@ -27,8 +27,11 @@ def getAURUpdates():
     
 def getSystemUpdates():
     command = "checkupdates"
-    #output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    output = subprocess.check_output(command,shell=True).decode("utf-8").split('\n')
+    try:
+        output = subprocess.check_output(command,shell=True).decode("utf-8").split('\n')
+    except subprocess.CalledProcessError as e:
+        print(e.cmd, "exited with non-zero exit code: ", e.returncode, file=sys.stderr)
+        return []
     updates = []
     for line in output:
         if line!='':
@@ -40,7 +43,7 @@ def formatData(updates):
         return "Up to date!"
     formatted = ""
     for update in updates:
-        formatted += update + "\n"        
+        formatted += "  " + update + "\n"        
     return formatted
     
 
@@ -50,50 +53,66 @@ class SystemTrayUpdateNotifier():
         self.style = self.app.style()
         self.icon = QtGui.QIcon(self.style.standardPixmap(QtGui.QStyle.SP_FileIcon))
         self.trayIcon = QtGui.QSystemTrayIcon(self.icon, parent)
-        self.menu = QtGui.QMenu(parent)
-
-        checkAction = self.menu.addAction("Check for updates", self.checkForUpdates)
-        self.menu.addAction("Display updates", self.displayUpdates)
-        exitAction = self.menu.addAction("Exit", self.app.quit)
         
+        # Build menu
+        self.menu = QtGui.QMenu(parent)
+        self.menu.addAction("Check for updates", self.checkForUpdates)
+        self.menu.addAction("Exit", self.app.quit)        
         self.trayIcon.setContextMenu(self.menu)
+        
         self.trayIcon.show()
         self.trayIcon.activated.connect(self.clickHandler)
         
+        # Timer to check for updates
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.checkForUpdates)
         self.timer.start(update_check_interval_minutes*60*1000)
         
+        # Timer to update tooltip every minute
+        self.tooltip_timer = QtCore.QTimer()
+        self.tooltip_timer.timeout.connect(self.setTooltip)
+        self.tooltip_timer.start(60*1000)
         
+        # Initial check for updates
         self.checkForUpdates()
         
         sys.exit(self.app.exec_())
         
     def checkForUpdates(self):
+        self.tooltip_timer.stop()
         self.system_updates = getSystemUpdates()
         self.aur_updates = getAURUpdates()
-        #self.lastChecked = time.now()
-        self.setTooltip()
+        self.lastChecked = 0
         self.displayUpdates()
+        self.setTooltip()
+        self.tooltip_timer.start(60*1000)
+        
         
     def setTooltip(self):
-        minutes = 0
-        tooltip = "Last checked " + str(minutes) + "minutes ago."
+        tooltip = "Last checked " + str(self.lastChecked) + " minutes ago."
         self.trayIcon.setToolTip(tooltip)
+        self.lastChecked += 1
+        
+    def updatesAvailable(self):
+        return len(self.system_updates) or len(self.aur_updates)
         
     def displayUpdates(self):
-        body = "System Updates:\n" + formatData(self.system_updates)
-        body += "\n\nAUR Updates:\n" + formatData(self.aur_updates)
-        print(formatData(self.system_updates))
-        print(formatData(self.aur_updates))
-        self.trayIcon.showMessage("Updates available!", body)
+        body = ""
+        title = ""
+        if self.updatesAvailable():
+            title = "Updates available!"
+            body += "System Updates:\n" + formatData(self.system_updates)
+            body += "\n\n"
+            body += "AUR Updates:\n" + formatData(self.aur_updates)
+        else:
+            title = "System up to date."
+            body += "No updates available."
+        self.trayIcon.showMessage(title, body)
         
     def clickHandler(self, reason):
         if reason == QtGui.QSystemTrayIcon.Trigger:
             self.displayUpdates()
-        
- 
-   
+  
 
 def main():
     updater = SystemTrayUpdateNotifier()
